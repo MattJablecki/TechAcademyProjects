a91efdf393e362a7385834acda2413d81dd5f99d
MattJablecki <46763228+MattJablecki@users.noreply.github.com>
2019-02-08 19:18:47 -0800

    Pizza Menu
    Pizza Menu files for Tech Academy project

---
+++ Pizza/final_drill_basic.js
@@ -0,0 +1,48 @@
+
+function getReceipt() {
+	// This initializes our string so it can get passed from  
+	// function to function, growing line by line into a full receipt
+	var text1 = "<h3>You Ordered:</h3>";
+	var runningTotal = 0;
+	var sizeTotal = 0;
+	var sizeArray = document.getElementsByClassName("size");
+	for (var i = 0; i < sizeArray.length; i++) {
+		if (sizeArray[i].checked) {
+			var selectedSize = sizeArray[i].value;
+			text1 = text1+selectedSize+"<br>";
+		}
+	}
+	if (selectedSize === "Personal Pizza") {
+		sizeTotal = 6;
+	} else if (selectedSize === "Medium Pizza") {
+		sizeTotal = 10;
+	} else if (selectedSize === "Large Pizza") {
+		sizeTotal = 14;
+	} else if (selectedSize === "Extra Large Pizza") {
+		sizeTotal = 16;
+	}
+	runningTotal = sizeTotal;
+	getVeggies(runningTotal,text1);
+};	
+
+function getVeggies(runningTotal,text1) {
+	var veggiesTotal = 0;
+	var selectedVeggies = [];
+	var veggiesArray = document.getElementsByClassName("veggies");
+	for (var j = 0; j < veggiesArray.length; j++) {
+		if (veggiesArray[j].checked) {
+			selectedVeggies.push(veggiesArray[j].value);
+			text1 = text1+veggiesArray[j].value+"<br>";
+		}
+	}
+	var veggiesCount = selectedVeggies.length;
+	if (veggiesCount > 1) {
+		veggiesTotal = (veggiesCount - 1);
+	} else {
+		veggiesTotal = 0;
+	}
+	runningTotal = (runningTotal + veggiesTotal);
+	document.getElementById("showText").innerHTML=text1;
+	document.getElementById("totalPrice").innerHTML = "</h3>Total: <strong>$"+runningTotal+".00"+"</strong></h3>";
+};	
+
